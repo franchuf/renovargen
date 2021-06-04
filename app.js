6586e39723d43c1889484f92c05b31381891d6ca
Francisco Faner <franchuf@gmail.com>
2021-06-04 13:09:41 -0300

    adding functions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //objeto global
 
 let db = {}
+let comments;
 
 //contructor de equipos
 
@@ -19,8 +20,9 @@ const agregarEquipo = (equipo) => {
     return equipo
 }
 
-const modificarEquipo  = (equipoId, {}) => {
-    return
+const addCommentsEquipment = (equipoId, comments) => {
+    db[equipoId].comentarios.push (comments)
+    return db[equipoId]
 }
 
 const modificarHsEquipo  = (equipoId, hours) => {
@@ -42,3 +44,11 @@ console.log(db)
 console.log(db['05078'])
 modificarHsEquipo('05078',3333);
 console.log(db['05078']);
+console.log(db['05073'].comentarios);
+addCommentsEquipment('05073',"comentarios222");
+addCommentsEquipment('05073',"comentarios333");
+addCommentsEquipment('05073',"comentarios444");
+addCommentsEquipment('05073',"comentarios555");
+
+console.log(db['05073'].comentarios);
+
